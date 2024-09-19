cbbed5f21076b8c685bf84d0591699cf39e29c0c
Juan Rosado <jjrosado91@gmail.com>
2024-09-19 19:22:17 +0200

    Laboratorio modulo 2

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+console.log("Total de la cena es de 120 €");
+console.log("La suma de las bebidas es de 18 €");
+console.log("A cada amigo le corresponde pagar", (120 - 18)/6, " €");
